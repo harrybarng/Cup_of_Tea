ae5eb68754feaac47f491764d70bcb13611cec24
monicapharm <maliuqing123@gmail.com>
2018-11-28 17:48:58 -0800

    read/write methods in database

--- app/src/main/java/edu/uw/barngh/cupoftea/FirebaseDB.kt
+++ app/src/main/java/edu/uw/barngh/cupoftea/FirebaseDB.kt
@@ -4,29 +4,43 @@ import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 
 
-
-
 class FirebaseDB {
-    var db = FirebaseFirestore.getInstance()
 
+    private var TAG = "database"
+
+    var db = FirebaseFirestore.getInstance()
 
     fun writeNewUser(userId: String) {
 
         val user = HashMap<String, Any>()
         user["first"] = "Monica"
         user["last"] = "Ma"
-        user["email"] = "gmial.com"
+        user["email"] = "gmail.com"
 
-        db.collection("users")
-            .add(user)
+        db.collection("users").document(userId)
+            .set(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    TAG,
+                    "A new user added with ID: " + userId
+                )
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "Error adding user", e) }
     }
 
-
-//    fun writeNewData(userId: String) {
-//        database = FirebaseDatabase.getInstance().reference
-//
-////        database.child("users").child(userId).setValue("hello")
-//        database.child("users").child(userId).child("username").setValue("Monica")
-//    }
-
+    fun readUser(userId: String) {
+        val docRef = db.collection("users").document(userId)
+        docRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document!!.exists()) {
+                    Log.d(TAG, "User data: " + document.data!!)
+                } else {
+                    Log.d(TAG, "No such user")
+                }
+            } else {
+                Log.d(TAG, "get failed with ", task.exception)
+            }
+        }
+    }
 }
--- app/src/main/java/edu/uw/barngh/cupoftea/NameActivity.kt
+++ app/src/main/java/edu/uw/barngh/cupoftea/NameActivity.kt
@@ -13,9 +13,15 @@ class NameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
 
+        // PLEASE DELETE ME, just for test
+        // test write new newer
         val DB = FirebaseDB()
-        DB.writeNewUser("123")
+        DB.writeNewUser("monica")
         Log.v("newDB", "loading db")
+        // test read from database
+        DB.readUser("monica")
+        Log.v("newDB", "loading user's data")
+
         findViewById<Button>(R.id.bt_get_started).setOnClickListener { v ->
             val intent = Intent(this, AgeActivity::class.java)
             this.startActivity(intent)
