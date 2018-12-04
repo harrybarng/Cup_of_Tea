6db37cf67069554f92779bb42b84fd0c90478011
harrybarng <37923379+harrybarng@users.noreply.github.com>
2018-12-03 21:22:39 -0800

    nav drawer

--- app/src/main/java/edu/uw/barngh/cupoftea/PersonListActivity.kt
+++ app/src/main/java/edu/uw/barngh/cupoftea/PersonListActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.preference.PreferenceManager
 import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.Toolbar
@@ -79,6 +82,21 @@ class PersonListActivity : AppCompatActivity() {
             setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
         }
 
+        var mDrawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+
 
         if (findViewById<View>(R.id.person_detail_container) != null) {
             this.mTwoPane = true
@@ -112,6 +130,16 @@ class PersonListActivity : AppCompatActivity() {
 
         loadData()
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                findViewById<DrawerLayout>(R.id.drawer_layout).openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun setFAB(fab_type: String) {
